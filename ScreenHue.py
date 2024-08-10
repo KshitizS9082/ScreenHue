92bd8bd02605d83f66715884831848234278767c
Kshitiz Sharma <kshitizs2809@gmail.com>
2024-08-11 03:16:19 +0530

    First commit

---
+++ ScreenHue.py
@@ -0,0 +1,74 @@
+"""
+RGB Bulb Device
+"""
+import time
+import tinytuya
+
+d = tinytuya.BulbDevice('d7f6cd1f04084457abbp5g', '192.168.1.2', "<~_YZ'Dfdk=cOh>e", version=3.4)
+d.set_version(3.4)  # IMPORTANT to set this regardless of version
+d.set_socketPersistent(True)  # Optional: Keep socket open for multiple commands
+data = d.status()
+
+# Show status of first controlled switch on device
+print('Dictionary %r' % data)
+
+# Set to RED Color - set_colour(r, g, b):
+d.turn_on()
+d.set_colour(255,244,0)  
+d.set_brightness_percentage(100)
+
+import mss
+from PIL import Image
+from colorthief import ColorThief
+import io
+def capture_screen():
+    with mss.mss() as sct:
+        # Capture a small portion of the screen (e.g., 200x200 pixels from the center)
+        monitor = {
+            "top": 540,    # 540px from the top of the screen
+            "left": 960,   # 960px from the left of the screen
+            "width": 200,  # 200px wide
+            "height": 200  # 200px tall
+        }
+        sct_img = sct.grab(monitor)
+        
+        # Convert the captured screen to a PIL image
+        img = Image.frombytes('RGB', (sct_img.width, sct_img.height), sct_img.rgb)
+        
+        return img
+
+def get_dominant_color(image):
+    # Save image to an in-memory file
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format='PNG')
+    img_byte_arr = img_byte_arr.getvalue()
+    
+    # Use ColorThief to get the dominant color
+    color_thief = ColorThief(io.BytesIO(img_byte_arr))
+    dominant_color = color_thief.get_color(quality=1)
+    
+    return dominant_color
+
+from colorsysx import rgb_to_hsv
+
+from math import sqrt
+old_color = (0, 0, 0)
+while True:
+    start = time.time()
+    screen_image = capture_screen()
+    dominant_color = get_dominant_color(screen_image)
+    if old_color == dominant_color:
+        continue
+        # pass
+    else:
+        # print("Changed colour to " + str(dominant_color))
+        r, g, b = dominant_color
+        d.set_colour(r,g,b)  
+        print("set to rgb")
+        # brigtness = sqrt((r*r+b*b+g*g)/3)*100/255
+        # d.set_brightness_percentage(brigtness)
+        print(f"New Color: {dominant_color}")
+        old_color = dominant_color
+    print("time taken: ", time.time()-start)
+    # Wait for a few seconds before the next capture
+    # time.sleep(1)  # Adjust the sleep time as needed
